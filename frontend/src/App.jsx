69cf03797d5eae1f2bdc8c48e9c71ddd39fdb793
aaryan <166105497+aaryankalra@users.noreply.github.com>
2025-06-27 23:38:30 +0530

    Initialized frontend
    Frontend initialized in ReactJS using Vite along with TailwindCSS and DaisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold">UniChance</h1>
+        <p className="text-xl">
+          Predict your chances at the world's best universities.
+        </p>
+        <button className="btn btn-success">Get Started</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
