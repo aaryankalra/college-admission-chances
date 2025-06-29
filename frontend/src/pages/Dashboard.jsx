5bc00755b9b64da5537ff8bebe35b1ee0a1abaae
aaryan <166105497+aaryankalra@users.noreply.github.com>
2025-06-29 17:06:37 +0530

    Added logout
    Logout functionality

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../utils/supabase";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [loading, setLoading] = useState(false);
+  const Navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      setLoading(true);
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        console.alert("Unable to logout.");
+        console.log(error);
+      }
+      Navigate("/");
+    } catch (error) {
+      console.alert("Unable to logout.");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <button
+          className="btn btn-error"
+          onClick={handleLogout}
+          disabled={loading}
+        >
+          {loading ? "Logging out..." : "Logout"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
